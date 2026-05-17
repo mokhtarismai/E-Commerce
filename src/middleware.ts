8230f98dfc62492d64e32c543b0d4e4f747fd54e
Mokhtar Ismai <mokhtarismail619431@gmail.com>
2026-05-18 02:19:58 +0300

    protect wishlist route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,29 @@
-import { getToken } from 'next-auth/jwt';
-import { NextRequest, NextResponse } from 'next/server'
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-    const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
-    const { pathname } = req.nextUrl
+  const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const { pathname } = req.nextUrl;
 
-    if (!jwt && (
-        pathname.startsWith('/profile') || 
-        pathname.startsWith('/orders') ||
-        pathname.startsWith('/checkout') ||
-        pathname.startsWith('/order')
-    )) {
-        return NextResponse.redirect(new URL('/login', req.url))
-    }
+  if (
+    !jwt &&
+    (pathname.startsWith("/profile") ||
+      pathname.startsWith("/wishlist") ||
+      pathname.startsWith("/orders") ||
+      pathname.startsWith("/checkout") ||
+      pathname.startsWith("/order"))
+  ) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
-    if (jwt && (pathname.startsWith('/login') || pathname.startsWith('/register'))) {
-        return NextResponse.redirect(new URL('/', req.url))
-    }
+  if (
+    jwt &&
+    (pathname.startsWith("/login") || pathname.startsWith("/register"))
+  ) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
@@ -26,6 +31,8 @@ export const config = {
         '/profile/:path*',
         '/login',
         '/register',
+        '/wishlist/:path*',
+        '/wishlist',
         '/orders/:path*',
         '/order',
         '/checkout',
