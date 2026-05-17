56b61b7ee09b8fa654592c58c9d4fede34351edc
Mokhtar Ismai <mokhtarismail619431@gmail.com>
2026-05-18 02:26:00 +0300

    fix middleware cookie name in production

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,32 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server'
 
 export default async function middleware(req: NextRequest) {
-  const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const { pathname } = req.nextUrl;
+    const jwt = await getToken({ 
+        req, 
+        secret: process.env.NEXTAUTH_SECRET,
+        cookieName: process.env.NODE_ENV === 'production' 
+            ? '__Secure-next-auth.session-token' 
+            : 'next-auth.session-token'
+    })
+    
+    const { pathname } = req.nextUrl
 
-  if (
-    !jwt &&
-    (pathname.startsWith("/profile") ||
-      pathname.startsWith("/wishlist") ||
-      pathname.startsWith("/orders") ||
-      pathname.startsWith("/checkout") ||
-      pathname.startsWith("/order"))
-  ) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+    if (!jwt && (
+        pathname.startsWith('/profile') || 
+        pathname.startsWith('/wishlist') ||
+        pathname.startsWith('/orders') ||
+        pathname.startsWith('/checkout') ||
+        pathname.startsWith('/order')
+    )) {
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
 
-  if (
-    jwt &&
-    (pathname.startsWith("/login") || pathname.startsWith("/register"))
-  ) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+    if (jwt && (pathname.startsWith('/login') || pathname.startsWith('/register'))) {
+        return NextResponse.redirect(new URL('/', req.url))
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
 }
 
 export const config = {
